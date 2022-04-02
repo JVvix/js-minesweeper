40b3e1cca3d11e249ed8af7e4df4ed6bed2e1c36
fayedotly@gmail.com <fayedotly@gmail.com>
2022-04-02 16:20:08 -0700

    added array of random numbers for mines

--- main.js
+++ main.js
@@ -1,27 +1,67 @@
+let clicks = 0
+let bombs = 0
+let amountOfMines = 40
+let lose = false
 const $board = $('#board');
-const ROWS = 10;
-const COLS = 10;
+const ROWS = 14;
+const COLS = 18;
+var cellNumber = 0
+
+var mines = []
+// console.log("test")
+
+  
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// console.log(getRandomInt(18 * 14))
+for (let i = 0; i < 40; i++) {
+  var number = getRandomInt(253);
+  if (mines.includes(number) == false) {
+    mines.push(number);
+  }
+}  
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function createBoard(rows, cols) {
   $board.empty();
-  for (let i = 0; i < rows; i++) {
+  for (let r = 0; r < rows; r++) {
     const $row = $('<div>').addClass('row');
-    for (let j = 0; j < cols; j++) {
+    for (let c = 0; c < cols; c++) {
+      
       const $col = $('<div>')
         .addClass('col hidden')
-        .attr('data-row', i)
-        .attr('data-col', j);
+        .attr('data-row', r)
+        .attr('data-col', c);
       if (Math.random() < 0.1) {
+    //  if (mines.includes(cellNumber)) {
+        console.log(cellNumber)
         $col.addClass('mine');
+        bombs += 1;
       }
       $row.append($col);
     }
     $board.append($row);
+    cellNumber++;
+  }
+}
+
+function goBack() { 
+  if (lose) {
+    
   }
 }
 
 function restart() {
+  clicks = 0
+  bombs = 0
   createBoard(ROWS, COLS);
+  document.getElementById('bombs').innerHTML = clicks;
+  document.getElementById('realbombs').innerHTML = bombs;
 }
 
 function gameOver(isWin) {
@@ -33,6 +73,7 @@ function gameOver(isWin) {
   } else {
     message = 'YOU LOST!';
     icon = 'fa fa-bomb';
+    lose = true
   }
   $('.col.mine').append(
     $('<i>').addClass(icon)
@@ -111,6 +152,8 @@ $board.on('click', '.col.hidden', function() {
     gameOver(false);
   } else {
     reveal(row, col);
+    clicks += 1
+    document.getElementById('bombs').innerHTML = clicks;
     const isGameOver = $('.col.hidden').length === $('.col.mine').length
     if (isGameOver) gameOver(true);
   }
